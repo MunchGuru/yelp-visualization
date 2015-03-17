bd3eaa7193e89257d0323b65259c5e6a1715f897
devmeyster <arthurfbi@yahoo.com>
2015-03-17 11:43:53 -0700

    added GET request

--- client/src/services/Services.js
+++ client/src/services/Services.js
@@ -2,7 +2,17 @@
 // Contains all our utility functions.
 define(function(require, exports, module) {
   var Util = {};
-  // Util.getData = function(){};
+  Util.getData = function() {
+		$.ajax(url, {
+	    contentType: 'application/json',
+	    success: function(response){
+	      console.log('Success! ', response);
+	    },
+	    error: function(response) {
+	      console.log(response);
+	    }
+  	});
+	};
   // Util.postData = function(data){};
   // etc.
 
