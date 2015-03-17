17913f11506ae21e2673613e4d415e0809dbcebf
devmeyster <arthurfbi@yahoo.com>
2015-03-17 11:46:13 -0700

    added POST request

--- client/src/services/Services.js
+++ client/src/services/Services.js
@@ -2,7 +2,7 @@
 // Contains all our utility functions.
 define(function(require, exports, module) {
   var Util = {};
-  Util.getData = function() {
+  Util.getData = function(url) {
 		$.ajax(url, {
 	    contentType: 'application/json',
 	    success: function(response){
@@ -13,8 +13,21 @@ define(function(require, exports, module) {
 	    }
   	});
 	};
-  // Util.postData = function(data){};
-  // etc.
+  var post = function(url, data) {
+  	$.ajax({
+        url: url,
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify(data),
+        success: function(response){
+
+          console.log("Success!", response);
+        },
+        error: function(err){
+          console.log(err);
+        }
+    });
+  };
 
   module.exports = Util;
 });
