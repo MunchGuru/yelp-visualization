560176adde9cefc0f28b9a20ca9869e3f535a2c3
Arian Faurtosh <arian@icloud.com>
2015-03-17 12:31:50 -0700

    removed post command

--- server/routers.js
+++ server/routers.js
@@ -2,9 +2,8 @@ var controllers = require('./controllers');
 var router = require('express').Router();
 
 for (var route in controllers) {
-  router.route("/" + route)
-    .get(controllers[route].get)
-    .post(controllers[route].post);
+  router.route('/' + route)
+    .get(controllers[route].get);
 }
 
 module.exports = router;
