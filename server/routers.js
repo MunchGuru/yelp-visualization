40bbee54634f2af4828746ca42e37c2541cb97d3
Arian Faurtosh <arian@icloud.com>
2015-03-22 00:55:52 -0700

    refactor routers to use yelper.js

--- server/routers.js
+++ server/routers.js
@@ -1,10 +1,7 @@
 var router = require('express').Router();
 var db = require('./db');
-var oauthSignature = require('oauth-signature');
-var n = require('nonce')();
-var request = require('request');
 
-
+var yelper = require('./yelper.js');
 
 var strip = function(fluff){
   'use strict';
@@ -15,7 +12,7 @@ var strip = function(fluff){
   return results;
 };
 
-router.route('/:cat?').get(function(req, res) {
+router.route('/:cat?/').get(function(req, res) {
   'use strict';
 
   
@@ -28,33 +25,7 @@ router.route('/:cat?').get(function(req, res) {
       if(results.length !== 0){
         res.json(strip(results));
       }else{
-        var httpMethod = 'GET';
-        var url = 'http://api.yelp.com/v2/search';
-        var parameters = {
-                oauth_consumer_key : '5vqhWS7N458VxObbh8FkZA',
-                oauth_token : 'YqLN73KG_trkg5IELGG90WJt0IsERj4U',
-                oauth_nonce : n(),
-                oauth_timestamp : n().toString().substr(0,10),
-                oauth_signature_method : 'HMAC-SHA1',
-                oauth_version : '1.0',
-                category_filter: req.params.cat,
-                location: 'San+Francisco',
-                sort: '2'
-            };
-        var consumerSecret = 'k2r5YixvkE8YfwvcZ0JpSi6Rj_o';
-        var tokenSecret = 'k6PDzaFsgQRjvgM7eDmCMhl08bI';
-        //     // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
-        var encodedSignature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret);
-
-        var percentEncode = function(s) {
-          return encodeURIComponent(s).replace(/\*/g, '%2A');
-        };
-
-
-        var apiURL = url+'?category_filter='+parameters.category_filter+'&sort='+parameters.sort+'&location='+percentEncode(parameters.location)+'&oauth_consumer_key='+parameters.oauth_consumer_key+'&oauth_token='+parameters.oauth_token+'&oauth_signature_method='+parameters.oauth_signature_method+'&oauth_version='+parameters.oauth_version+'&oauth_nonce='+parameters.oauth_nonce+'&oauth_timestamp='+parameters.oauth_timestamp+'&oauth_signature='+encodedSignature;
-        
-        console.log(apiURL);
-        request(apiURL, function(error, response, body){
+        yelper.request_yelp({category_filter: req.params.cat}, function(error, response, body){
           if (!error && response.statusCode === 200) {
             res.json(JSON.parse(body));
           }else{
@@ -62,7 +33,6 @@ router.route('/:cat?').get(function(req, res) {
             res.end('please notify arian about this\n'+body);
           }
         });
-
       }
     });
   });
