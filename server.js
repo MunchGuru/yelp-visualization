9ba31931be72c0b2c7812d187e832be631240dca
Arian Faurtosh <arian@icloud.com>
2015-03-23 10:00:01 -0700

    removed api keys and set up error checking

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 
 var app = require('./server/app');
 
-app.listen(app.get("port"));
-console.log("Listening on", app.get("port"));
+
+//----------//
+// Warnings //
+//----------//
+
+if(process.env.oauth_consumer_key === undefined){
+  console.log('WARNING:  `process.env.oauth_consumer_key` is not set!');
+}
+
+if(process.env.oauth_token === undefined){
+  console.log('WARNING:  `process.env.oauth_consumer_key` is not set!');
+}
+
+if(process.env.consumerSecret === undefined){
+  console.log('WARNING:  `process.env.consumerSecret` is not set!');
+}
+
+if(process.env.tokenSecret === undefined){
+  console.log('WARNING:  `process.env.tokenSecret` is not set!');
+}
+
+
+app.listen(app.get('port'));
+console.log('Listening on', app.get('port'));
