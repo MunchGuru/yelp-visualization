6696b42c8ab4703ac61c9543e1c10a09061f2c20
Kuni Natsuki <knatsuki92@gmail.com>
2015-03-23 11:36:25 -0700

    refactored itemView

--- client/src/views/ItemView.js
+++ client/src/views/ItemView.js
@@ -9,6 +9,7 @@ define(function(require, exports, module) {
     var StateModifier = require('famous/modifiers/StateModifier');
     var Easing = require('famous/transitions/Easing');
 
+    var ItemSurface = require('views/ItemSurface');
     
     function ItemView(data) {
         View.apply(this, arguments);
@@ -20,7 +21,7 @@ define(function(require, exports, module) {
 
         // events from itemSurface is piped to the ItemView
         itemSurface.pipe(this);
-        // When a ItemView receives a 'click' event, it then fires a 'dummyClick' event
+        // When a ItemView receives a 'click' event, it then fires a 'itemClick' event
         // which is then listened by AppView.
         this._eventInput.on('click', function() {
           this._eventOutput.emit('itemClick', this.options);
@@ -31,117 +32,6 @@ define(function(require, exports, module) {
     ItemView.prototype.constructor = ItemView;
 
     ItemView.DEFAULT_OPTIONS = {};
-
-
-
-    // ItemSurface get instantiated inside ItemView
-    function ItemSurface(itemData) {
-
-        //This is for easy refactoring for variable names
-        var data = {};
-        //if you are a final result
-        if(itemData.name){
-            data.name = itemData.name;
-            data.stars = itemData.rating || 2.5;
-            data.disc = itemData.disc || 'No Details at this time.';
-            data.url = itemData.url || 'https://www.google.com';
-            data.pic = itemData.image_url || undefined;
-            data.phoneNumber = itemData.display_phone || 'No Phone Number Available';
-            data.top = itemData.top;
-            data.left = itemData.left;
-            data.size = itemData.size || 200;
-        }
-        //if you are a seletion or filter
-        else{
-            console.log(itemData)
-        }
-        var css = {
-            backgroundColor: 'rgb(240, 238, 233)',
-            
-            textAlign: 'center',
-            padding: '5px',
-            border: '5px solid rgba(255,255,255, 0.35)',
-            'border-radius': '250px',
-            '-webkit-filter':'grayscale(50%) sepia(0.2) brightness(125%)',
-            filter: 'grayscale(50%) sepia(0.2) brightness(125%)'
-            
-          }
-        
-
-        var advancedSurface = new Surface({
-          content: "<h4>"+data.name+"</h4>\
-                    <div>" + data.phoneNumber + "</div>\
-                    <a href='"+data.url+"'>link to website</a>\
-                    <p>"+data.disc+"</p>",
-
-          size: [data.size,data.size],
-          properties: css
-        });
-
-        //sets the background image if it is available
-        if(data.pic){
-            css['background-image'] = 'url("'+data.pic+'")';
-            css['background-size'] = data.size+'px';
-        }
-        var starsString = ''
-        for (var i = 0; i < Math.floor(data.stars); i++) {
-            starsString += '<img style="width:20px;height:20px;" src="./src/views/img/star.png"/>';
-        };
-        if(data.stars%1){
-            starsString += '<img style="width:20px;height:20px;" src="./src/views/img/star-half.png"/>'
-        }
-        console.log(data.stars, starsString)
-        var basicSurface = new Surface({
-          content: "<h3>"+data.name+"</h3>" + starsString,
-          size: [data.size,data.size],
-          properties: css
-        });
-
-
-
-        /*
-        ----------------------
-        View
-        ----------------------
-        */
-        var frontView = new View();
-        frontView.add(basicSurface);
-        
-        
-        var backView = new View();
-        backView.add(advancedSurface);
-
-
-        var flipper = new Flipper();
-        flipper.setFront(frontView);
-        flipper.setBack(backView);
-
-
-        var myView = new View();
-        myView.add(flipper);
-
-        basicSurface.pipe(myView);
-        advancedSurface.pipe(myView);
-        frontView.pipe(myView);
-        backView.pipe(myView);
-
-
-        if(location){
-            myView._eventInput.on('click', function() {
-                flipper.flip();
-            });
-        }else{
-            myView.pipe(this);
-            this._eventInput.on('click', function() {
-              this._eventOutput.emit('dummyClick', this.options);
-            }.bind(this));
-        }
-
-        
-
-        return myView;
-    }
-
 
     module.exports = ItemView;
 
